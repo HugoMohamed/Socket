517da52c4b71416292fd2e3f86e1fc0f4404afb8
Hugo <ihfisk42@gmail.com>
2019-02-01 15:20:13 -0500

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,161 @@
+/********************
+*                   *
+*	Hugo MOHAMED	*
+*					*
+********************/
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+// Need to link with Ws2_32.lib, Mswsock.lib, and Advapi32.lib
+#pragma comment (lib, "Ws2_32.lib")
+#pragma comment (lib, "Mswsock.lib")
+#pragma comment (lib, "AdvApi32.lib")
+
+
+#define DEFAULT_BUFLEN 512
+#define DEFAULT_PORT "27015"
+
+int main(int argc, char **argv)
+{
+	WSADATA wsaData;
+	SOCKET ConnectSocket = INVALID_SOCKET;
+	struct addrinfo *result = NULL,
+		*ptr = NULL,
+		hints;
+	char *sendbuf = "this is a test";
+	char recvbuf[DEFAULT_BUFLEN];
+	int iResult;
+	int recvbuflen = DEFAULT_BUFLEN;
+
+	// Validate the parameters
+  //  if (argc != 2) {
+		//cout << "usage: " << argv[0] <<  " server-name" << endl;
+  //      return;
+  //  }
+
+	// Initialize Winsock
+	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != 0) {
+		cout << "WSAStartup failed with error: " << iResult << endl;
+		return -1;
+	}
+
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;  // Address Format Unspecified or AF_INET (for IPv4)
+	hints.ai_socktype = SOCK_STREAM;  // Provides sequenced, reliable, two-way, connection-based byte streams 
+	hints.ai_protocol = IPPROTO_TCP;  // Indicates that we will use TCP
+
+	// Resolve the server address and port
+	//       => defines port number and gets remote IP address (in "result" struc)
+	//iResult = getaddrinfo(argv[1], DEFAULT_PORT, &hints, &result);
+	iResult = getaddrinfo("localhost", DEFAULT_PORT, &hints, &result);
+	if (iResult != 0) {
+		cout << "getaddrinfo failed with error: " << iResult << endl;
+		WSACleanup();
+		return -1;
+	}
+
+	// Attempt to connect to an address until one succeeds
+	for (ptr = result; ptr != NULL; ptr = ptr->ai_next) {
+
+		// Create a SOCKET for connecting to server
+		ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype,
+			ptr->ai_protocol);
+		if (ConnectSocket == INVALID_SOCKET) {
+			cout << "socket failed with error: " << WSAGetLastError() << endl;
+			WSACleanup();
+			return -1;
+		}
+
+		// Connect to server.
+		iResult = connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+		// Note:  "ai_addr" is a struct containing IP address and port
+		if (iResult == SOCKET_ERROR) {
+			closesocket(ConnectSocket);
+			ConnectSocket = INVALID_SOCKET;
+			continue;
+		}
+		break;
+	}
+
+	freeaddrinfo(result);
+
+	if (ConnectSocket == INVALID_SOCKET) {
+		cout << "Unable to connect to server!" << endl;
+		WSACleanup();
+		return -1;
+	}
+
+	// Send the whole content of sendbuf (15 bytes including '\0')
+	iResult = send(ConnectSocket, sendbuf, (int)strlen(sendbuf) + 1, 0);
+	if (iResult == SOCKET_ERROR) {
+		cout << "send failed with error: " << WSAGetLastError() << endl;
+		closesocket(ConnectSocket);
+		WSACleanup();
+		return -1;
+	}
+
+	cout << "Bytes Sent: " << iResult << endl;
+
+	// Shutdown the connection since no more data will be sent.
+	// This will cause the other party (the server) to exit any recv() function waiting for incoming data.
+	// The recv() in the server program will then return the value 0.
+	iResult = shutdown(ConnectSocket, SD_SEND);
+	if (iResult == SOCKET_ERROR) {
+		cout << "shutdown failed with error: " << WSAGetLastError() << endl;
+		closesocket(ConnectSocket);
+		WSACleanup();
+		return -1;
+	}
+
+	bool done;
+	int i, cumul;
+
+	// Receive until the peer shuts down the connection
+	do {
+		// IMPORTANT: The server will send a C string terminated by '\0' (not a C++ string) 
+		//            We will therefore loop until we receive '\0'
+		done = false;
+		cumul = 0;
+		while (!done) {
+			iResult = recv(ConnectSocket,
+				&recvbuf[cumul],
+				recvbuflen,    // not the number of bytes to be received !!!
+				0);
+			if (iResult != 0) { // if connection has not been shutdown (some bytes received)
+				for (i = cumul; i < cumul + iResult; i++) {
+					if (recvbuf[i] == '\0')
+						done = true;
+				}
+				cumul += iResult;  // increment the byte counter
+			}
+			else {  // if connection has been shutdown by the other party (the server), quit the loop 
+				done = true;
+			}
+		}
+
+		if (cumul > 0) {
+			cout << "Bytes received: " << iResult << endl;
+		}
+		else if (cumul == 0)
+			cout << "Connection closed" << endl;
+		else
+			cout << "recv failed with error: " << WSAGetLastError() << endl;
+
+	} while (cumul > 0);
+
+	// cleanup
+	closesocket(ConnectSocket);
+	WSACleanup();
+
+	return 0;
+}
---
+++ server.cpp
@@ -0,0 +1,182 @@
+/********************
+*                   *
+*	Hugo MOHAMED	*
+*					*
+********************/
+
+#undef UNICODE
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+// Need to link with Ws2_32.lib
+#pragma comment (lib, "Ws2_32.lib")
+#pragma comment (lib, "Mswsock.lib")
+
+#define DEFAULT_BUFLEN 512
+#define DEFAULT_PORT "27015"
+
+int main(void)
+{
+	WSADATA wsaData;  //  WSA stands for "Windows Socket API"
+	int iResult;
+	fstream image;
+	image.open("./image.png", fstream::binary);
+
+	SOCKET ListenSocket = INVALID_SOCKET;
+	SOCKET ClientSocket = INVALID_SOCKET;
+
+	struct addrinfo *result = NULL;
+	struct addrinfo hints;
+
+	int iSendResult;
+	char recvbuf[DEFAULT_BUFLEN];
+	int recvbuflen = DEFAULT_BUFLEN;
+
+	// Initialize Winsock
+	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != 0) {
+		cout << "WSAStartup failed with error: " << iResult << endl;
+		return -1;
+	}
+
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_INET;  // Address Format Internet
+	hints.ai_socktype = SOCK_STREAM;  //  Provides sequenced, reliable, two-way, connection-based byte streams
+	hints.ai_protocol = IPPROTO_TCP;  // Indicates that we will use TCP
+	hints.ai_flags = AI_PASSIVE;  // Indicates that the socket will be passive => suitable for bind()
+
+	// Resolve the server address and port
+	//       => defines port number and gets local IP address (in "result" struc)
+	iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
+	if (iResult != 0) {
+		cout << "getaddrinfo failed with error: " << iResult << endl;
+		WSACleanup();
+		return -1;
+	}
+
+	// Create a SOCKET for connecting to server
+	//     (note that we could have defined the 3 socket parameter using "hints"...)
+	ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+	if (ListenSocket == INVALID_SOCKET) {
+		cout << "socket failed with error: " << WSAGetLastError() << endl;
+		freeaddrinfo(result);
+		WSACleanup();
+		return -1;
+	}
+
+	// Setup the TCP listening socket
+	//     (for "bind()", we need the info returned by "getaddrinfo()")
+	iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
+	// Note:  "ai_addr" is a struct containing IP address and port
+	if (iResult == SOCKET_ERROR) {
+		cout << "bind failed with error: " << WSAGetLastError() << endl;
+		freeaddrinfo(result);
+		closesocket(ListenSocket);
+		WSACleanup();
+		return -1;
+	}
+
+	freeaddrinfo(result);
+
+	// define the maximum number of connections
+	iResult = listen(ListenSocket, SOMAXCONN);
+	if (iResult == SOCKET_ERROR) {
+		cout << "listen failed with error: " << WSAGetLastError() << endl;
+		closesocket(ListenSocket);
+		WSACleanup();
+		return -1;
+	}
+
+	// Accept a client socket
+	//   Notes:
+	//          1- "accept()" is a blocking function
+	//          2- It returns a new socket of the connection
+	ClientSocket = accept(ListenSocket, NULL, NULL);
+	if (ClientSocket == INVALID_SOCKET) {
+		cout << "accept failed with error: " << WSAGetLastError() << endl;
+		closesocket(ListenSocket);
+		WSACleanup();
+		return -1;
+	}
+
+	// No longer need server socket
+	closesocket(ListenSocket);
+
+	bool done;
+	int i, cumul;
+
+	// Receive until the peer shuts down the connection using the shutdown() function.
+	do {
+		// IMPORTANT: The client will send a C string terminated by '\0' (not a C++ string)
+		//            We will therefore loop until we receive '\0'
+		done = false;
+		cumul = 0;
+		while (!done) {
+			iResult = recv(ClientSocket,
+				&recvbuf[cumul],
+				recvbuflen,  // not the number of bytes to be received !!!
+				0);
+			if (iResult != 0) { // if connection has not been shutdown (some bytes received)
+				for (i = cumul; i < cumul + iResult; i++) {
+					if (recvbuf[i] == '\0')
+						done = true;
+				}
+				cumul += iResult; // increment the byte counter
+			}
+			else {  // if connection has been shutdown by the other party (the client), quit the loop 
+				done = true;
+			}
+		}
+
+		if (cumul > 0) {
+			cout << "Bytes received: " << cumul << endl;
+
+			// Echo the content of "recvbuf" back to the sender
+			// Therefore, we have to send "cumul" bytes 
+			iSendResult = send(ClientSocket, recvbuf, cumul, 0);
+			if (iSendResult == SOCKET_ERROR) {
+				cout << "send failed with error: " << WSAGetLastError() << endl;
+				closesocket(ClientSocket);
+				WSACleanup();
+				return -1;
+			}
+			cout << "Bytes sent: " << iSendResult << endl;
+		}
+		else if (cumul == 0)
+			cout << "Connection closing..." << endl;
+		else {
+			cout << "recv failed with error: " << WSAGetLastError() << endl;
+			closesocket(ClientSocket);
+			WSACleanup();
+			return -1;
+		}
+
+	} while (cumul > 0);
+
+	// Shutdown the connection since we're done.
+	// This will cause the other party (the client) to exit any recv() function waiting for incoming data.
+	// The recv() in the client program will then return the value 0.
+	iResult = shutdown(ClientSocket, SD_SEND);
+	if (iResult == SOCKET_ERROR) {
+		cout << "shutdown failed with error: " << WSAGetLastError() << endl;
+		closesocket(ClientSocket);
+		WSACleanup();
+		return -1;
+	}
+
+	// cleanup
+	closesocket(ClientSocket);
+	WSACleanup();
+
+	return 0;
+}
